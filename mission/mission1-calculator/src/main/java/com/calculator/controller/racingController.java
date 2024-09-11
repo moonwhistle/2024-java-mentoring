274335ba5dbbb90e835cdf3c6c930e53ae22f518
승엽 <tmdduqflfl@naver.com>
2024-09-11 11:14:46 +0900

    Feat : git test

---
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/racingController.java
@@ -0,0 +1,12 @@
+package com.calculator.controller;
+
+import com.calculator.view.Inputview;
+
+public class racingController {
+
+    private Inputview inputview;
+
+    public racingController(final Inputview inputview) {
+        this.inputview = inputview;
+    }
+}
