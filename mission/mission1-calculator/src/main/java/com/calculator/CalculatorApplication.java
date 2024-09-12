2cc89e374c66d54931cd3c1aff93839dd3c73ef4
daeyeon <kdy4410121@naver.com>
2024-09-12 16:54:28 +0900

    feat: Main문 작성

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,8 +1,16 @@
 package com.calculator;
 
+import com.calculator.controller.CalculatorController;
+import com.calculator.domain.StringCalculator;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
 public class CalculatorApplication {
-
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
+        CalculatorController Controller = new CalculatorController(inputView, outputView);
+        Controller.startCalculate();
     }
 }
