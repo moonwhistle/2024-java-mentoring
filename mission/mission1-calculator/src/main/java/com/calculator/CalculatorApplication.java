b2d6c5001b11d08fdacefa1186bbff2c9236dee7
parkjiwon530 <dojang120@mju.ac.kr>
2024-09-20 03:24:55 +0900

    Refactor: 리드미 삭제

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,7 +1,14 @@
 package com.calculator;
 
+import com.calculator.controller.CalculatorController;
+
+
 public class CalculatorApplication {
 
+
     public static void main(String[] args) {
+        CalculatorController calculatorController = new CalculatorController();
+        calculatorController.result();
     }
+
 }
